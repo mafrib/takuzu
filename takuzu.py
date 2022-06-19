9d49de8caa5a9b04ef0c3bf60f36ad8aa6975fc4
Carlos Vaz <carlos@carjorvaz.com>
2022-06-19 12:27:26 +0100

    Change internal board representation to tuple of tuples

--- takuzu.py
+++ takuzu.py
@@ -36,9 +36,9 @@ class TakuzuState:
 class Board:
     """Representação interna de um tabuleiro de Takuzu."""
 
-    def __init__(self, n):
+    def __init__(self, board):
         """Inicializa um tabuleiro vazio."""
-        self.board = np.full((n, n), 2)
+        self.board = board
 
     def __len__(self):
         """Devolve a dimensão (N) do tabuleiro N x N"""
@@ -48,25 +48,34 @@ class Board:
         """Retorna representação externa do tabuleiro"""
         return "\n".join("\t".join(map(str, row)) for row in self.board)
 
-    def update(self, row: int, col: int, number: int):
-        """Atualiza o valor de uma posição no tabuleiro."""
-        self.board[row, col] = number
-
-    def copy(self):
-        """Devolve uma cópia profunda do tabuleiro"""
-        newBoard = Board(len(self.board))
+    def move_result(self, row: int, col: int, number: int):
+        """Devolve o tabuleiro resultante da jogada recebida."""
+        newBoard = ()
         for i in range(len(self.board)):
-            for j in range(len(self.board)):
-                newBoard.update(i, j, self.get_number(i, j))
-        return newBoard
+            if i == row:
+                newRow = ()
+                for j in range(len(self.board)):
+                    if j == col:
+                        newRow += (number,)
+                    else:
+                        newRow += (self.board[i][j],)
+                newBoard += (newRow,)
+            else:
+                newBoard += (self.board[i],)
+
+        return Board(newBoard)
 
     def get_row(self, row: int):
-        """Devolve tuplo contendo a respetiva linha do tabuleiro"""
-        return tuple(self.board[row])
+        """Devolve a respetiva linha do tabuleiro"""
+        return self.board[row]
 
     def get_col(self, col: int):
-        """Devolve tuplo contendo a respetiva coluna do tabuleiro"""
-        return tuple(self.board[:, col])
+        """Devolve a respetiva coluna do tabuleiro"""
+        tup = ()
+        for i in range(len(self.board)):
+            tup += (self.board[i][col],)
+
+        return tup
 
     def get_rows(self):
         """Devolve tuplo contendo as linhas do tabuleiro"""
@@ -90,27 +99,27 @@ class Board:
 
     def get_number(self, row: int, col: int) -> int:
         """Devolve o valor na respetiva posição do tabuleiro."""
-        return self.board[row, col]
+        return self.board[row][col]
 
     def adjacent_vertical_numbers(self, row: int, col: int) -> (int, int):
         """Devolve os valores imediatamente abaixo e acima,
         respectivamente."""
         if row == 0:
-            return (self.board[row + 1, col], None)
+            return (self.board[row + 1][col], None)
         elif row == len(self.board) - 1:
-            return (None, self.board[row - 1, col])
+            return (None, self.board[row - 1][col])
         else:
-            return (self.board[row + 1, col], self.board[row - 1, col])
+            return (self.board[row + 1][col], self.board[row - 1][col])
 
     def adjacent_horizontal_numbers(self, row: int, col: int) -> (int, int):
         """Devolve os valores imediatamente à esquerda e à direita,
         respectivamente."""
         if col == 0:
-            return (None, self.board[row, col + 1])
+            return (None, self.board[row][col + 1])
         elif col == len(self.board) - 1:
-            return (self.board[row, col - 1], None)
+            return (self.board[row][col - 1], None)
         else:
-            return (self.board[row, col - 1], self.board[row, col + 1])
+            return (self.board[row][col - 1], self.board[row][col + 1])
 
     def check_equal_adjacent(self, row: int, col: int) -> int:
         """Devolve o número a preencher caso os números adjacentes sejam
@@ -245,32 +254,19 @@ class Board:
             > stdin.readline()
         """
 
-        # TODO voltar a mudar para a submissão
         from sys import stdin
 
         n = int(stdin.readline())
-        board = Board(n)
+        board = ()
 
         for i in range(n):
-            line = tuple(map(int, stdin.readline().split()))
+            row = ()
+            input_row = tuple(map(int, stdin.readline().split()))
             for j in range(n):
-                board.update(i, j, line[j])
+                row += (input_row[j],)
+            board += (row,)
 
-        return board
-
-        # # TODO remover; só para testes locais
-        # import fileinput
-        # read = list(fileinput.input("./tests/input_T01"))
-        # n = int(read[0])
-        # read = read[1:]
-        # board = Board(n)
-
-        # for i in range(n):
-        #     line = tuple(map(int, read[i].split()))
-        #     for j in range(n):
-        #         board.update(i, j, line[j])
-
-        # return board
+        return Board(board)
 
     # TODO: outros metodos da classe
 
@@ -360,10 +356,7 @@ class Takuzu(Problem):
         'state' passado como argumento. A ação a executar deve ser uma
         das presentes na lista obtida pela execução de
         self.actions(state)."""
-
-        newState = TakuzuState(state.board.copy())
-        newState.board.update(action[0], action[1], action[2])
-        return newState
+        return TakuzuState(state.board.move_result(action[0], action[1], action[2]))
 
     def goal_test(self, state: TakuzuState):
         """Retorna True se e só se o estado passado como argumento é
